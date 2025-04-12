4912bac3da07b9e1e5193134cbbea51e2d6c4b6e
Aashish518 <jadavaashish9@gmail.com.com>
2025-04-12 18:28:35 +0530

    complet

--- BACKEND/Database/db.js
+++ BACKEND/Database/db.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectToMongo = () => {
-mongoose.connect("mongodb://localhost:27017/FINDBOOKS",{
+    mongoose.connect("mongodb+srv://jadavaashish9:An%40211518@cluster0.gtxxzpc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(()=>{
         console.log("connection successfully");
-    }).catch(()=>{
+}).catch((error) => {
+        console.log(error)
         console.log("not connected");
     })
 }
--- BACKEND/index.js
+++ BACKEND/index.js
@@ -10,10 +10,10 @@ dotenv.config();
 
 
 app.use(cors({
-    origin: "http://localhost:5173", 
-    credentials: true, 
-    methods: ["GET", "POST", "PUT", "DELETE"], 
-    allowedHeaders: ["Content-Type", "Authorization"], 
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -34,6 +34,6 @@ app.use("/api", require("./Routes/SellOrders"));
 app.use("/api", require("./Routes/Payment"));
 app.use('/api/report', reportRoutes);
 
-app.listen(PORT,'0.0.0.0',() => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`your application run at http://localhost:${PORT}`);
 })
